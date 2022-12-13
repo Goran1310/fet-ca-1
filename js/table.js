ff9aae2f7d8d8d2f2da5181815d178b731f46624
Goran Lovincic <66091550+Goran1310@users.noreply.github.com>
2022-12-13 04:06:23 +0100

    test2

--- js/table.js
+++ js/table.js
@@ -1,77 +1,78 @@
+// Use this file to populate the Table in the ‘personal’ section as follows, using Vanilla JavaScript:
 
+// You need to have 5 rows of data.
+// Populate the table with data of your choice that is relevant to the table headers.
+// The Table needs to be populated from the 'table.js' file you have created
 
 var array = [
-    ['#3214', '</>', 'description','info'],
-    ['#3215', '</img>', 'description','info'],
-    ['#3216', '</>', 'description','info'],
-    ['#3217', '</>', 'description','info'],
-    ['#3218', '</>', 'description','info'],
+    ['Car', '4 wheels', '180km/h','17k$'],
+    ['Train', '2 wheels', '200km/h', '13m$'],
+    ['Bike', '2 wheels', '30km/h', '1k$'],
+    ['Airplane', '3 wheels', '350km/h', '43M$'],
+    ['Truck', '6 wheels', '90km/h', '130k$']
   ] // Creating a data array which a loop will source from
 
-var table = document.getElementById("personalTable")
-// document.body.appendChild(table); // Drew the main table node on the document
-
+var table = document.getElementById("#personalTable");
+let tbody = document.getElementsByTagName("tbody")[0];
+tbody.style.border = "thick solid #0000FF";
 array.forEach(function(row) {
-  var tr = table.insertRow(); //Create a new row
-  var img = document.createElement('img');
- 
+  var tr = tbody.insertRow(); //Create a new row
+  const td = document.createElement("td");// Drew the main table node on the document
+  const img = document.createElement("img");
   row.forEach(function(column) {
     var td = tr.insertCell();
     tr.appendChild(img);
     td.innerText = column; // Take string from placeholder variable and append it to <tr> node
   });
 });
-const tr = document.querySelectorAll("tr");
-const td = document.createElement("td");
-const img = document.querySelectorAll("img");
+let img = document.getElementsByTagName("img")
+img[1].setAttribute("src", "./img/van-2-150x150.webp")
+img[2].setAttribute("src", "./img/train-6-150x150.webp")
+img[3].setAttribute("src", "./img/mountain-bike-4-150x150.webp")
+img[4].setAttribute("src", "./img/airplane-3-150x150.webp")
+img[5].setAttribute("src", "./img/dump-truck-3-150x150.webp")
 
-// img[0].src = "https://picsum.photos/200/301"; // image 1
-img[1].src = "./img/170-200x302.jpeg"; // image 2
-img[2].src = "./img/42-200x305.jpeg"; // image 3
-img[3].src = "./img/277-200x304.jpeg"; // image 4
-img[4].src = "./img/341-200x304.jpeg"; // image 5
-img[5].src = "./img/927-200x304.jpeg"; // image 5
-img[1].setAttribute("id","img1");
-img[2].setAttribute("id","img2");
-img[3].setAttribute("id","img3");
-img[4].setAttribute("id","img4");
-img[5].setAttribute("id","img5");
+//  The following functionality needs to be implemented in the ‘table.js’ file, using jQuery:
 
-// Functionality needs to be implemented in the ‘table.js’ file, using jQuery
 // When one of the images in the Table is clicked, the image needs to be displayed in a new window in your web browser.
-$("#img1").click(function(){
-    window.open("./img/170-200x302.jpeg", 'coffe bar', 'width=452,height=425 top=200,left=600');
-    return false;
-  });
-$("#img2").click(function(){
-    window.open("./img/42-200x305.jpeg", 'coffe bar', 'width=452,height=425 top=200,left=600');
-    return false;
-  });
-$("#img3").click(function(){
-    window.open("./img/277-200x304.jpeg", 'coffe bar', 'width=452,height=425 top=200,left=600');
-    return false;
-  });
-$("#img4").click(function(){
-    window.open("./img/341-200x304.jpeg", 'coffe bar', 'width=452,height=425 top=200,left=600');
-    return false;
-  });
-$("#img5").click(function(){
-    window.open("./img/927-200x304.jpeg", 'coffe bar', 'width=452,height=425 top=200,left=600');
-    return false;
-  });
-
-//   The table’s odd rows need to change font color when the mouse hovers over them
-$('#personalTable tr:odd').hover(function() {
-    $(this).addClass("hover");
-    $(this).css("color", "blue");
-}, function() {
-    $(this).removeClass('hover');
-    $(this).css("color", "black");
+img[1].setAttribute("id", "img1");
+img[2].setAttribute("id", "img2");
+img[3].setAttribute("id", "img3");
+img[4].setAttribute("id", "img4");
+img[5].setAttribute("id", "img5");
+const windowFeatures = "left=100,top=100,width=320,height=320";
+$('#img1').click(function(){
+  window.open("./img/van-2-150x150.webp", 'window name', windowFeatures);
+  return false;
 });
-// changing font color back to black once the mouse stops hovering over them.
-$('#personalTable tr:odd').click(function()
-  {  
-    $(this).removeClass('hover');
-    $(this).css("color", "black");
-  });
+$('#img2').click(function(){
+  window.open("./img/train-6-150x150.webp", 'window name', windowFeatures);
+  return false;
+});
+$('#img3').click(function(){
+  window.open("./img/mountain-bike-4-150x150.webp", 'window name', windowFeatures);
+  return false;
+});
+$('#img4').click(function(){
+  window.open("./img/airplane-3-150x150.webp", 'window name', windowFeatures);
+  return false;
+});
+$('#img5').click(function(){
+  window.open("./img/dump-truck-3-150x150.webp", 'window name', windowFeatures);
+  return false;
+});
+// The table’s odd rows need to change font color when the mouse hovers over them, changing font color back to black once the mouse stops hovering over them.
+$( "tr:odd" ).hover(
+  function() {
+    $( this ).addClass( "hover" );
+    $( this ).children().first().css( "background-color", "#D3D3D3" );
+    $( this ).children().last().css( "background-color", "#D3D3D3" );
+    $( this ).css( "color", "blue" );
+  }, function() {
+    $( this ).removeClass( "hover" );
+    $( this ).css( "color", "black" );
+    $( this ).children().first().css( "background-color", "" );
+    $( this ).children().last().css( "background-color", "" );
 
+  }
+);
--- js/welcome.js
+++ js/welcome.js
@@ -1,21 +1,29 @@
+// create a jQuery function that selects the HTML element with the “liveToastBtn” ID and shows the HTML Toast element with the ID of ‘liveToast’. 
+// This function should be run when the user clicks the ‘Like’ button in the HTML header.
+$(document).ready(function(){
+    $("#liveToastBtn").click(function(){
+        // console.log("click on liveToast")
+        $("#liveToast").show();
+    });
+  });
+
+// Make a copy of the ‘Like’ button element in the HTML file and place it directly after
+//  the existing ‘Like’ button element. Change the button text to “I’m Bored” and the element’s ID to “boredBtn”. 
+// In the same ‘welcome.js’ file, create another function that executes a GET Request to the “https://www.boredapi.com/api/activity/” API, once the new “I’m Bored” button is clicked.
+//  The response from this API Request should be logged into the web console.
+
+$(document).ready(function(){
+    $("#boredBtn").click(function(){
+        var request = new XMLHttpRequest();
+        request.open('GET', "https://www.boredapi.com/api/activity/");
+        request.onload = function() {
+	    var response = request.response;
+	    var parsedData = JSON.parse(response);
+	    console.log(parsedData);
+        }
+        request.send();
+    });
+  });
 
 
-$("#liveToastBtn").click(function() {
-    $("#liveToast").show();
-})
-
-
-$("#boredBtn").click(function() {
-    var request = new XMLHttpRequest();
-
-    request.open('GET', 'https://www.boredapi.com/api/activity/');
-    
-    request.onload = function() {
-       var response = request.response
-       var parsedData = JSON.parse(response);
-       console.log(parsedData);
-    }
-    
-    request.send();
-})
 
